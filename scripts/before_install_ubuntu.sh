e0866b18413bfa4ee2d079c119d7dee4ba399abf
faizal <faizal@Faizals-MBP>
2024-09-07 21:24:22 +0400

    commit issue

--- scripts/before_install_ubuntu.sh
+++ scripts/before_install_ubuntu.sh
@@ -11,3 +11,11 @@ sudo apt install mysql-server -y
 # Install PHP 8.3 and required extensions
 sudo apt install php-fpm php-mysql -y
 #sudo apt install php-mbstring php8-xml 
+
+sudo systemctl restart nginx
+
+# Start the PHP service
+sudo systemctl restart php-fpm
+
+# Ensure MySQL is running
+sudo systemctl restart mysql
